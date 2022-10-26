d022ae4cdc3305e153d0a1ca7526afc4a0a1c41f
LeeDongjun <adgjl1125@neowoori.com>
2022-10-26 21:39:06 +0900

    commit!

---
+++ djtv_now/src/main/java/kr/co/djtv/DjtvNowApplication.java
@@ -0,0 +1,13 @@
+package kr.co.djtv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DjtvNowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DjtvNowApplication.class, args);
+	}
+
+}
---
+++ djtv_now/src/test/java/kr/co/djtv/DjtvNowApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.djtv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DjtvNowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
